679180ecfa2cea95d600132d7f46cc9ed8418da6
Amir <kazemi.amir68@gmail.com>
2022-08-07 14:17:44 +0200

    fix hover/unhover effect on text inside portfolio items

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -5,8 +5,8 @@ const PortfolioItem = ({ children, title }) => {
   return (
     <Card style={{}} classname="mt-16 p-0">
       <div className="w-full h-full overflow-hidden relative group">
-        <div className="card-image w-full h-full "></div>
-        <span className="absolute top-[50%] text-white text-xl left-[50%] -translate-x-[50%] -translate-y-96 transition-all duration-500  group-hover:scale-125 group-hover:-translate-y-[50%]">
+        <div className="card-image w-full h-full group-hover:scale-125 group-hover:blur"></div>
+        <span className="absolute top-[50%] text-white text-xl left-[50%] -translate-x-[50%] -translate-y-96 transition-all duration-500  group-hover:scale-125 group-hover:-translate-y-[50%] hover:-translate-y-[50%]">
           {title}
         </span>
       </div>
