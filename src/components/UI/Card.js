5a967df0f145da4e8343c38878f0bfc83932c7e2
Amir <kazemi.amir68@gmail.com>
2022-08-07 14:32:40 +0200

    making portfoliopage responsive

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ classname, children, style }) => {
   const classes =
-    "w-[80vw] h-[30vh] bg-white rounded-lg shadow-xl ring-1 ring-gray-900/5 mx-auto sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 " +
+    "w-[80vw] h-[30vh] bg-white rounded-lg shadow-xl ring-1 ring-gray-900/5 mx-auto " +
     classname;
   return (
     <div className={classes} style={style}>
