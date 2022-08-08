4656540550546aac77ac5a1da0fd5aee0fd9060b
Amir <kazemi.amir68@gmail.com>
2022-08-08 15:17:54 +0200

    add heading title to portfolio page

--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -3,10 +3,13 @@ import Card from "../UI/Card";
 
 const PortfolioItem = ({ children, title }) => {
   return (
-    <Card style={{}} classname="mt-16 sm:w-[90%] md:w-[70%]">
+    <Card
+      style={{}}
+      classname="mt-16 sm:w-[90%] md:w-[90%] lg:w-[90%] xl:w-[70%]"
+    >
       <div className="w-full h-full overflow-hidden relative group rounded-lg">
         <div className="card-image w-full h-full group-hover:scale-125 group-hover:blur"></div>
-        <span className="absolute top-[50%] text-white text-xl left-[50%] -translate-x-[50%] -translate-y-96 transition-all duration-500  group-hover:scale-125 group-hover:-translate-y-[50%] hover:-translate-y-[50%]">
+        <span className="absolute  text-white text-xl top-[50%] left-[50%] -translate-x-[50%] -translate-y-96 transition-all duration-500  group-hover:scale-125 group-hover:-translate-y-[50%] hover:-translate-y-[50%]">
           {title}
         </span>
       </div>
--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -4,11 +4,16 @@ import { portfolioData } from "../utils/data";
 
 const Portfolio = () => {
   return (
-    <section className="sm:grid sm:grid-cols-2 md:grid-cols-3 gap-0 md:px-16 md:w-[70%] md:mx-auto">
-      {portfolioData.map((item, index) => {
-        return <PortfolioItem key={index} title={item.title} />;
-      })}
-    </section>
+    <div>
+      <h1 className="text-center font-black text-red-600 font-header text-5xl">
+        Completed Projects
+      </h1>
+      <section className="sm:grid sm:grid-cols-2 md:grid-cols-3 gap-0 md:px-16 md:w-[100%] md:mx-auto lg:w-[70%]">
+        {portfolioData.map((item, index) => {
+          return <PortfolioItem key={index} title={item.title} />;
+        })}
+      </section>
+    </div>
   );
 };
 
