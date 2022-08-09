ef216134f6c2f05e0cf560da93c075655e1c28dc
Amir <kazemi.amir68@gmail.com>
2022-08-09 20:35:05 +0200

    adding transition menu

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import NavMenu from "../components/UI/NavMenu";
+import Bio from "../components/about/Bio";
+import Skills from "../components/about/Skills";
+import SocialMedias from "../components/about/SocialMedias";
+import { CSSTransition } from "react-transition-group";
+
+const initialMenuData = [
+  { title: "About", isActive: true, comp: <Bio /> },
+  { title: "Skills", isActive: false, comp: <Skills /> },
+  { title: "Social medias", isActive: false, comp: <SocialMedias /> },
+];
 
 const About = () => {
+  const [activeComp, setActiveComp] = useState(<Bio />);
+  const showActiveContentHandler = (component) => {
+    setActiveComp(component);
+  };
+
   return (
     <section>
-      <NavMenu />
+      <NavMenu
+        menuData={initialMenuData}
+        onTabChange={showActiveContentHandler}
+      >
+        <CSSTransition
+          in={activeComp !== null}
+          timeout={500}
+          classNames="abouts"
+        >
+          <div></div>
+        </CSSTransition>
+      </NavMenu>
     </section>
   );
 };
