1c5f728606323607c2064140bb239211d8f4de8b
Amir <kazemi.amir68@gmail.com>
2022-08-09 19:17:00 +0200

    making skills section responsive

--- src/components/about/SkillItem.js
+++ src/components/about/SkillItem.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const SkillItem = ({ title, percentage }) => {
   return (
-    <div className="flex justify-between mb-2 p-4 border-2 border-gray-700 overflow-scroll rounded-lg">
-      <h1 className="font-body text-xl font-black">{title}</h1>
+    <div className="flex flex-col items-center justify-between mb-4 p-4 border-2 border-gray-700 rounded-lg md:flex-row">
+      <h1 className="font-body text-lg font-black ">{title}</h1>
       <progress
         value={percentage}
         max="100"
-        className="text-3xl rounded-xl p-2 accent-red-600"
+        className="text-lg rounded-xl p-2 w-[90%] h-8 md:w-[50%]"
       >
         {percentage}%
       </progress>
